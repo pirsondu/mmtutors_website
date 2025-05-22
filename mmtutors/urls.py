2ee0c43b607c3711031181d0c16465883ee0c867
Ye Htut Win <yehtutwin.tech@gmail.com>
2025-05-22 23:27:13 +0630

    brand name correction

--- mmtutors/urls.py
+++ mmtutors/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_header = "MMTutors Admin"
-admin.site.site_title = "MMTutors Admin Portal"
-admin.site.index_title = "Welcome to MMTutors Portal"
+admin.site.site_header = "MMtutors Admin"
+admin.site.site_title = "MMtutors Admin Portal"
+admin.site.index_title = "Welcome to MMtutors Portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
